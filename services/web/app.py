9fad1b62e477e3045c8fe5b5ad6844bb70285296
t4rk <t4rk@outlook.com>
2021-07-14 10:43:09 -0400

    :wrench: Remove parse call.

--- services/web/app.py
+++ services/web/app.py
@@ -1,11 +1,7 @@
-import os
-import sys
-
+# App from dazzler readme.
 from dazzler import Dazzler
 from dazzler.system import Page, BindingContext, transforms as t
 from dazzler.components import core
-
-sys.path.insert(0, '.')
 
 app = Dazzler(__name__)
 page = Page(
@@ -52,12 +48,7 @@ app.add_page(page)
 
 
 async def app_factory():
+    # Set the static directory only for deployment.
     app.config.requirements.static_directory = '/home/app/web/assets'
-
-    await app._on_parse(app.cli.parser.parse_args([]))
-    app.logger.warn(f'backend {app.config.session.backend}')
-    app.logger.warn(f'CWD: {os.getcwd()}')
-    app.logger.info(f'LS: {os.listdir(os.getcwd())}')
-    app.logger.info(f'CONFIG_PATH: {app.config_path}')
     return await app.application()
 
